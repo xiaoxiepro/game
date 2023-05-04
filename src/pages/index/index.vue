2ebdf4b986dd89a4e762aed905bbb0083e8a8f3d
xcy <2327016463@qq.com>
2023-05-04 22:03:34 +0800

    tj

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="index-page">
+    <Header />
+  </view>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.index-page {
+  width: 100%;
+  min-height: 100vh;
+  background: url("@/static/imgs/page-bg.png") 0 0/100% 100%;
+}
+</style>
